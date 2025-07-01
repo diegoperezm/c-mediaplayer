e7bfbe0a0af980d672833b1ff2004765e3f4928f
diegoperezm <diegoperezcorreo@gmail.com>
2025-07-01 11:14:49 -0400

    Remove int offset

--- src/main.c
+++ src/main.c
@@ -28,7 +28,6 @@ int main(int argc, char **argv) {
 
     char *file_paths[MAX_FILEPATH_RECORDED] =  {0};
     int file_path_counter = 0;
-    int offset  = 0; 
 
     for(int i = 0; i < MAX_FILEPATH_RECORDED; i++){
       file_paths[i] =  (char *) RL_CALLOC(MAX_FILEPATH_SIZE,1);
@@ -52,7 +51,7 @@ int main(int argc, char **argv) {
         ClearBackground(BGCOLOR);
         grid_layout(&media_player, pipeline, file_paths, file_path_counter);
       EndDrawing();
-    }
+    } // end: while
 
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
@@ -60,6 +59,9 @@ int main(int argc, char **argv) {
     for(int i=0; i < MAX_FILEPATH_RECORDED; i++){
        RL_FREE(file_paths[i]);
     }
+
+    // free???: GuiLoadStyleCyber()
+   //  GuiLoadStyleDefault();
 
     CloseWindow();
     return EXIT_SUCCESS;
